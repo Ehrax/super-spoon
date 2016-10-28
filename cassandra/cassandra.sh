8a06afbf33a6a51e3ffeecb6a01b7b5aaa4bd033
Alexander Rasputin <alexander.rasputin@uni-ulm.de>
2016-10-28 10:38:42 +0200

    did some fixes

--- cassandra/cassandra.sh
+++ cassandra/cassandra.sh
@@ -5,10 +5,13 @@
 declare -A IPS
 # add Main cluster node as last
 # declare as following ([host_name]=host_ip
-IPS=([host_name]=host_ip)
+IPS=(["script-test"]="134.60.64.235")
 
 USER='ubuntu' # maschine user, Default: Ubuntu
 SSH_KEY='my_key' # path to youre key
 
+# link to cassandra binary
+CASSANDRA_BINARY='http://archive.apache.org/dist/cassandra/2.2.6 \
+    /apache-cassandra-2.2.6-bin.tar.gz'
 
 
--- couchbase/couchbase.sh
+++ couchbase/couchbase.sh
@@ -6,7 +6,7 @@
 
 declare -A IPS
 # add Main cluster node as last
-# declare as following ([host_name]=host_ip
+# declare as following (["host_name"]="host_ip"
 IPS=([host_name]=host_ip)
 
 USER='ubuntu' # maschine user, Default: Ubuntu
